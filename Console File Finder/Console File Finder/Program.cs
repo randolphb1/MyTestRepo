4f9e0a84a01aa4bcd152b2395e57d694c535ddab
Bradley R. Randolph <e x a l t e d m o r o n  at  g m a i l . c o m>
2013-02-12 16:20:50 -0500

    Initial version of console file finder utility
    There may be a library missing

---
+++ Console File Finder/Console File Finder/Program.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Security;
+using System.Threading;
+using File_Finder_Library;
+using WelchAllyn.ProgressBarForm;
+
+namespace Console_File_Finder
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class CMyOptions
+    {
+        public CMyOptions()
+        {
+            fOptionLocalDrivesOnly = false;
+            fJustFilenames = false;
+            myArgs = new List<String>();
+            aOptionDrives = new List<String>();
+        }
+
+        public bool fOptionLocalDrivesOnly;
+        public bool fJustFilenames;
+        public List<String> myArgs;
+        public List<String> aOptionDrives;
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    class WorkerThread
+    {
+        /// <summary>
+        /// A forms-based progress bar
+        /// </summary>
+        static private ProgressBarForm pbf;
+        static private ProgressBarSink pbs;
+        /// <summary>
+        /// The main worker thread
+        /// </summary>
+        /// <param name="options">A CMyOptions object</param>
+        public static void ThreadFunc(Object options)
+        {
+            CMyOptions opts = (CMyOptions) options;
+            SuperFileFinderDriver ffd = new SuperFileFinderDriver();
+            List<String> availableDrives = ffd.GetDrives( opts.fOptionLocalDrivesOnly );
+            List<String> drives = new List<String>();
+            DateTime start = DateTime.Now;
+
+            if ( opts.aOptionDrives.Count > 0 )
+            {
+                String str;
+
+                foreach ( String o in opts.aOptionDrives )
+                {
+                    str = o;
+
+                    if ( o == "." )
+                    {
+                        str = ffd.GetCurrentDirectory();
+
+                        if ( !str.EndsWith( "\\" ) )
+                        {
+                            str += "\\";
+                        }
+                    }
+
+                    foreach ( String d in availableDrives )
+                    {
+                        if ( str.ToUpper().Contains( d.ToUpper() ) )
+                        {
+                            try
+                            {
+                                System.IO.DirectoryInfo di = new System.IO.DirectoryInfo( str );
+
+                                if ( di.Exists )
+                                {
+                                    drives.Add( str );
+                                }
+                            }
+                            catch ( SecurityException se )
+                            {
+                                Console.Error.WriteLine( se.ToString() );
+                            }
+
+                            break;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                drives = availableDrives;
+            }
+
+            if ( drives.Count > 0 )
+            {
+                int folder_count = 0;
+
+                if ( opts.fJustFilenames == false )
+                {
+                    Console.WriteLine( "Searching for filenames containing [{0}] on drive{1} {2}",
+                                        String.Join( " | ", opts.myArgs.ToArray() ).ToUpper(),
+                                        drives.Count > 1 ? "s" : "",
+                                        String.Join( ",", drives.ToArray() ).ToUpper() );
+                }
+
+                pbf = new ProgressBarForm();
+                pbf.Show( 1000 );
+                pbs = new ProgressBarSink( pbf );
+
+                foreach ( String driveLetter in drives )
+                {
+                    folder_count += ffd.GetFoldersCount( driveLetter, pbs );
+                }
+
+                pbf.SetMax( folder_count );
+                pbf.SetValue( 0 );
+
+                foreach ( String driveLetter in drives )
+                {
+                    ffd.FindFolders( driveLetter, opts.myArgs, opts.fJustFilenames, pbs );
+                }
+
+                pbf.Hide();
+            }
+            else
+            {
+                Console.Error.WriteLine( "ERROR: Specified drives or folders do not exist- {0}",
+                                         String.Join( ", ", opts.aOptionDrives.ToArray() ).ToUpper() );
+            }
+
+            if ( opts.fJustFilenames == false )
+            {
+                DateTime end = DateTime.Now;
+                TimeSpan diff = end - start;
+                Console.WriteLine( "{0} files found\nExecution time: {1}", ffd.Count, diff );
+            }
+        }
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            if ( args.Length > 0 )
+            {
+                CMyOptions myOpts = new CMyOptions();
+
+                for ( int nn = 0; nn < args.Length; nn++ )
+                {
+                    if ( args[ nn ].ToLower() == "-lo" )		// Local-only options
+                    {
+                        myOpts.fOptionLocalDrivesOnly = true;
+                    }
+                    else if ( args[ nn ].ToLower() == "-fo" )	// Files-only option
+                    {
+                        myOpts.fJustFilenames = true;
+                    }
+                    else if ( args[ nn ].Contains( ":" ) )		// Is a drive letter specified?
+                    {
+                        if ( !args[ nn ].EndsWith( "\\" ) )		// make sure it ends with "\\"
+                        {
+                            args[ nn ] += "\\";
+                        }
+
+                        myOpts.aOptionDrives.Add( args[ nn ] );
+                    }
+                    else if ( args[ nn ] == "." )				// Is 'current directory' specified?
+                    {
+                        myOpts.aOptionDrives.Add( args[ nn ] );
+                    }
+                    else
+                    {
+                        char[] argN = args[ nn ].ToLower().ToCharArray();
+
+                        if ( argN[ 0 ] == '-' || argN[ 0 ] == '/' )
+                        {
+                            if ( argN[ 1 ] == '?' || argN[ 1 ] == 'h' )
+                            {
+                                Assembly assem = Assembly.GetExecutingAssembly();
+                                AssemblyName assemName = assem.GetName();
+                                Assembly assem_fflib = Assembly.Load("File Finder Library");
+                                AssemblyName assemNamefflib = assem_fflib.GetName();
+                                Assembly assem_pbar = Assembly.Load( "ProgressBarForm" );
+                                AssemblyName assemNamepbar = assem_pbar.GetName();
+
+                                Console.WriteLine( "CONSOLE FILE FINDER, version {0}", assemName.Version.ToString( 3 ) );
+                                Console.WriteLine( "    File Finder Lib, version {0}", assemNamefflib.Version.ToString( 3 ) );
+                                Console.WriteLine( "   Progress Bar Lib, version {0}", assemNamepbar.Version.ToString( 3 ) );
+                                Console.WriteLine( "USAGE:  cff [-lo] [-fo] [[d1: ... dN:] | [d:\\path\\] | [.]] patt-1 ... patt-N" );
+                                Console.WriteLine( "MAIN OPTIONS:" );
+                                Console.WriteLine( " -lo             Restrict search to locally attached drives" );
+                                Console.WriteLine( " -fo             Just report on file pathnames (no stats)" );
+                                Console.WriteLine( " d1: ... dN:     One or more drives to search, or" );
+                                Console.WriteLine( " d:\\path\\        specifies the drive letter and path to start searching, or" );
+                                Console.WriteLine( " . (dot)         specifies to search the current folder," );
+                                Console.WriteLine( "                 otherwise when nothing specified searches all available drives" );
+                                Console.WriteLine( " patt-N          One or more strings to search for in the filename" );
+#if DEBUG
+                                Console.ReadLine();
+#endif
+                                return;
+                            }
+                        }
+                        else
+                        {
+                            myOpts.myArgs.Add( args[ nn ] );
+                        }
+                    }
+                }
+
+                if ( myOpts.myArgs.Count > 0 )
+                {
+                    Thread thd = new Thread( new ParameterizedThreadStart( WorkerThread.ThreadFunc ) );
+
+                    thd.Priority = ThreadPriority.AboveNormal;
+                    thd.Start( myOpts );
+                    thd.Join();
+                }
+                else
+                {
+                    Console.Error.WriteLine( "WARNING: No filename pattern specified." );
+                }
+            }
+            else
+            {
+                Console.Error.WriteLine( "ERROR: No arguments specified." );
+            }
+        }
+    }
+}
---
+++ Console File Finder/File Finder Library/FileFinderLib.cs
@@ -0,0 +1,560 @@
+﻿// File Finder Library (in C-sharp)
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using WelchAllyn.ProgressBarInterface;
+
+namespace File_Finder_Library
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class FoundFileInfo
+    {
+        private String _name;
+        private DateTime _datetime;
+        private long _length;
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="nn"></param>
+        /// <param name="dt"></param>
+        /// <param name="ll"></param>
+        public FoundFileInfo( String nn, DateTime dt, long ll )
+        {
+            _name = nn;
+            _datetime = dt;
+            _length = ll;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public String Name
+        {
+            get
+            {
+                return _name;
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public String Time
+        {
+            get
+            {
+                return _datetime.ToString();
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public String Length
+        {
+            get
+            {
+                return _length.ToString();
+            }
+        }
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    public class FileInfoComparer : IComparer<FoundFileInfo>
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public int Compare( FoundFileInfo x, FoundFileInfo y )
+        {
+            if ( null == x )
+            {
+                if ( null == y )
+                {
+                    return 0;
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+            else
+            {
+                if ( null == y )
+                {
+                    return 1;
+                }
+                else
+                {
+                    int rv = x.Name.Length.CompareTo( y.Name.Length );
+
+                    if ( rv != 0 )
+                    {
+                        return rv;
+                    }
+                    else
+                    {
+                        return x.Name.CompareTo( y.Name );
+                    }
+                }
+            }
+        }
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    public class FileFinderHelper
+    {
+        /// <summary>
+        /// Retreives a list of logical drive names attached to this computer.
+        /// </summary>
+        /// <param name="fLocalOnly">Limits the list of drives names to those that are phyically attached.</param>
+        /// <returns>A list of strings containing the logical drive names.</returns>
+        public List<String> GetDrives( bool fLocalOnly )
+        {
+            DriveInfo[] myDrives = DriveInfo.GetDrives();
+            List<String> rvDrives = new List<String>();
+
+            foreach ( DriveInfo d in myDrives )
+            {
+                if ( fLocalOnly )
+                {
+                    switch ( d.DriveType )
+                    {
+                        case DriveType.Network:
+                            /* fall through */
+                        case DriveType.NoRootDirectory:
+                            /* fall through */
+                        case DriveType.Unknown:
+                            continue;	// Not a local drive
+                        case DriveType.Ram:
+                        /* fall through */
+                        case DriveType.CDRom:
+                        /* fall through */
+                        case DriveType.Fixed:
+                        /* fall through */
+                        case DriveType.Removable:
+                        /* fall through */
+                        default:
+                            break;		// A local drive
+                    }
+                }
+
+                if ( d.IsReady )
+                {
+                    rvDrives.Add( d.Name );
+                }
+            }
+
+            rvDrives.Sort();
+
+            return rvDrives;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public List<String> GetFolders( String root )
+        {
+            DirectoryInfo myDir = new DirectoryInfo( root );
+            DirectoryInfo[] myFolders;
+            List<String> rvFolders = new List<String>();
+
+            try
+            {
+                myFolders = myDir.GetDirectories();
+            }
+            catch //( Exception ex )
+            {
+                //Console.Error.WriteLine( "ERROR: {0}", ex.Message );
+
+                return rvFolders; // return an empty list
+            }
+
+            foreach ( DirectoryInfo di in myFolders )
+            {
+                if ( FileAttributes.System != (di.Attributes & FileAttributes.System) )
+                {
+                    rvFolders.Add( di.Name );
+                }
+            }
+
+            rvFolders.Sort();
+
+            return rvFolders;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="branch"></param>
+        /// <returns></returns>
+        public List<FoundFileInfo> GetFiles( String branch )
+        {
+            DirectoryInfo myDir = new DirectoryInfo( branch );
+            FileInfo[] myFiles;
+            List<FoundFileInfo> rvFiles = new List<FoundFileInfo>();
+
+            try
+            {
+                myFiles = myDir.GetFiles();
+            }
+            catch //( Exception ex )
+            {
+                //Console.Error.WriteLine( "ERROR: {0}", ex.Message );
+                return rvFiles; // return an empty list;
+            }
+
+            foreach ( FileInfo fi in myFiles )
+            {
+                if ( FileAttributes.System != (fi.Attributes & FileAttributes.System) )
+                {
+                    rvFiles.Add( new FoundFileInfo( fi.Name, fi.CreationTime, fi.Length ) );
+                }
+            }
+
+            rvFiles.Sort(new FileInfoComparer());
+
+            return rvFiles;
+        }
+        /// <summary>
+        /// Get the current working directory name.
+        /// </summary>
+        /// <returns>A string containing the name of the current working directory.</returns>
+        public String GetCurrentDirectory()
+        {
+            String cwd;
+
+            try
+            {
+                cwd = Directory.GetCurrentDirectory();
+            }
+            catch ( UnauthorizedAccessException uae )
+            {
+                Console.Error.WriteLine( "ERROR: {0}", uae.ToString() );
+                cwd = null;
+            }
+
+            return cwd;
+        }
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    public class FileFinderDriver
+    {
+        private FileFinderHelper _finder;
+        private long _count_of_files_found;
+        /// <summary>
+        /// Constructors
+        /// </summary>
+        public FileFinderDriver()
+        {
+            _finder = new FileFinderHelper();
+            _count_of_files_found = 0L;
+        }
+        /// <summary>
+        /// Constructor #2
+        /// </summary>
+        /// <param name="fflib"></param>
+        public FileFinderDriver( FileFinderHelper ff )
+        {
+            _finder = ff;
+            _count_of_files_found = 0L;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public long Count
+        {
+            get
+            {
+                return _count_of_files_found;
+            }
+
+            set
+            {
+                if (0 > value)
+                {
+                    value = 0L;
+                }
+                
+                _count_of_files_found = value;
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="branch"></param>
+        /// <param name="seeds"></param>
+        /// <param name="justnames"></param>
+        public void FindFiles( String branch, List<String> seeds, bool justnames )
+        {
+            List<FoundFileInfo> files = _finder.GetFiles( branch );
+
+            foreach ( FoundFileInfo file in files )
+            {
+                foreach ( String s in seeds )
+                {
+                    if ( file.Name.ToLower().Contains( s.ToLower() ) )
+                    {
+                        if ( justnames == false )
+                        {
+                            Console.Write( "{0,7},{1,22},", file.Length, file.Time );
+                        }
+
+                        Console.WriteLine( "{0}", Path.Combine( branch, file.Name ) );
+                        _count_of_files_found++;
+                        break;
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="seeds"></param>
+        /// <param name="pbn"></param>
+        public void FindFolders( String root, List<String> seeds, bool fJustFiles, IProgressBarNotification pbn )
+        {
+            List<String> folders = _finder.GetFolders( root );
+
+            if ( null != pbn )
+            {
+                pbn.IncrementBar( folders.Count );
+            }
+
+            FindFiles( root, seeds, fJustFiles );
+
+            foreach ( String folder in folders )
+            {
+                String newRoot = Path.Combine( root, folder );
+                FindFolders( newRoot, seeds, fJustFiles, pbn );
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="branch"></param>
+        /// <param name="seeds"></param>
+        /// <param name="dest"></param>
+        private void GetFilesHelper( String branch, String[] seeds, ref List<String> dest )
+        {
+            List<FoundFileInfo> files = _finder.GetFiles( branch );
+
+            foreach ( FoundFileInfo file in files )
+            {
+                foreach ( String s in seeds )
+                {
+                    if ( file.Name.ToLower().Contains( s.ToLower() ) )
+                    {
+                        dest.Add( Path.Combine( branch, file.Name ) );
+                        _count_of_files_found++;
+                        break;
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="seeds"></param>
+        /// <param name="dest"></param>
+        private void GetFileFoldersHelper( String root, String[] seeds, ref List<String> dest )
+        {
+            GetFilesHelper( root, seeds, ref dest );
+            List<String> folders = _finder.GetFolders( root );
+
+            foreach ( String folder in folders )
+            {
+                String newRoot = Path.Combine( root, folder );
+                GetFileFoldersHelper( newRoot, seeds, ref dest );
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="seeds"></param>
+        /// <returns></returns>
+        public List<String> GetFilePaths( String root, String[] seeds )
+        {
+            List<String> rvFoundFilePaths = new List<String>();
+
+            _count_of_files_found = 0L;
+            GetFileFoldersHelper( root, seeds, ref rvFoundFilePaths );
+
+            return rvFoundFilePaths;
+        }
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    public class SuperFileFinderDriver : FileFinderHelper
+    {
+        private long _count_of_files_found;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public SuperFileFinderDriver()
+        {
+            _count_of_files_found = 0L;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public long Count
+        {
+            get
+            {
+                return _count_of_files_found;
+            }
+
+            set
+            {
+                if ( 0 > value )
+                {
+                    value = 0L;
+                }
+
+                _count_of_files_found = value;
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="branch"></param>
+        /// <param name="seeds"></param>
+        /// <param name="justnames"></param>
+        public void FindFiles( String branch, List<String> seeds, bool justnames )
+        {
+            List<FoundFileInfo> files = GetFiles( branch );
+
+            foreach ( FoundFileInfo file in files )
+            {
+                foreach ( String s in seeds )
+                {
+                    if ( file.Name.ToLower().Contains( s.ToLower() ) )
+                    {
+                        if ( justnames == false )
+                        {
+                            Console.Write( "{0,7},{1,22},", file.Length, file.Time );
+                        }
+
+                        Console.WriteLine( "{0}", Path.Combine( branch, file.Name ) );
+                        _count_of_files_found++;
+                        break;
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="seeds"></param>
+        /// <param name="pbn"></param>
+        public void FindFolders( String root, List<String> seeds, bool fJustFiles, IProgressBarNotification pbn )
+        {
+            List<String> folders = GetFolders( root );
+            
+            if ( null != pbn )
+            {
+                pbn.IncrementBar( folders.Count );
+            }
+
+            FindFiles( root, seeds, fJustFiles );
+
+            foreach ( String folder in folders )
+            {
+                String newRoot = Path.Combine( root, folder );
+                FindFolders( newRoot, seeds, fJustFiles, pbn );
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="branch"></param>
+        /// <param name="seeds"></param>
+        /// <param name="dest"></param>
+        private void GetFilesHelper( String branch, String[] seeds, ref List<String> dest )
+        {
+            List<FoundFileInfo> files = GetFiles( branch );
+
+            foreach ( FoundFileInfo file in files )
+            {
+                foreach ( String s in seeds )
+                {
+                    if ( file.Name.ToLower().Contains( s.ToLower() ) )
+                    {
+                        dest.Add( Path.Combine( branch, file.Name ) );
+                        _count_of_files_found++;
+                        break;
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="seeds"></param>
+        /// <param name="dest"></param>
+        private void GetFileFoldersHelper( String root, String[] seeds, ref List<String> dest )
+        {
+            GetFilesHelper( root, seeds, ref dest );
+            List<String> folders = GetFolders( root );
+
+            foreach ( String folder in folders )
+            {
+                String newRoot = Path.Combine( root, folder );
+                GetFileFoldersHelper( newRoot, seeds, ref dest );
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="seeds"></param>
+        /// <returns></returns>
+        public List<String> GetFilePaths( String root, String[] seeds )
+        {
+            List<String> rvFoundFilePaths = new List<String>();
+
+            _count_of_files_found = 0L;
+            GetFileFoldersHelper( root, seeds, ref rvFoundFilePaths );
+
+            return rvFoundFilePaths;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns>The number of folders found</returns>
+        public int GetFoldersCount( String root, IProgressBarNotification pbn )
+        {
+            List<String> folders = GetFolders( root );
+            int count_of_folders = folders.Count;
+
+            if ( null != pbn )
+            {
+                pbn.IncrementBar( count_of_folders );
+            }
+
+            foreach ( String folder in folders )
+            {
+                String newRoot = Path.Combine( root, folder );
+                count_of_folders += GetFoldersCount( newRoot, pbn );
+            }
+
+            return count_of_folders;
+        }
+    }
+}
